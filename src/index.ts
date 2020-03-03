95f080c325af50912ee314a8d60e5d7fc897e8f0
Ian0714 <54196835+Ian0714@users.noreply.github.com>
2020-03-03 10:05:27 +0900

    createBlock

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,28 @@
-class human{
-    public name: string;
-    public age: number;
-    public gender: string;
-    constructor(name: string, age: number, gender?: string){
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+    constructor(
+        index: number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number
+    ){
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
 
-const ian = new human("Ian", 21, "male")
+const genesisBlock:Block = new Block(0, "", "", "", 0);
 
-const sayHi = (person: human): string => {
-    return (`hello ${person.name} you are ${person.age} and ${person.gender}!`);
-}
+let blockchain: [Block] = [genesisBlock];
 
-console.log(sayHi(ian));
+console.log(blockchain);
 
 export {};
